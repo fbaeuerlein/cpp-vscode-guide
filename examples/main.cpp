582e01fdacc8e372d48eed9e0a7445cdf1992380
Florian Baeuerlein <florian@abstrus.es>
2020-08-25 20:31:59 +0200

    added .clang-format

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include "include/MyClass1.hxx"
+#include <iostream>
 
 int main(int, char **) // NOLINT
 {
     std::cout << "Hello, world!\n";
-    
+
     MyClass1<int> m(0);
 
     return m.value();
-
 }
-
